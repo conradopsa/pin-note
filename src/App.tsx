6dc4c2d1d8423770c11913b98f3969ce959b9a93
Conrado Pinheiro <conradopsa@gmail.com>
2020-11-02 12:23:56 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+import { IconPlus, IconMenu2, IconList, IconGrid } from 'tabler-icons'
+import iconHorse from './assets/icon/horse.svg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface ICard {
+  title: string,
+  text: string
+}
+
+export interface AppProps {
+
+}
+
+export interface AppState {
+  cards: ICard[]
+}
+
+class App extends React.Component<AppProps, AppState> {
+  state: AppState = { cards: [] }
+
+  cards: ICard[] = [];
+
+  insertNote(e: any) {
+    this.cards.push({ title: "Seu título...", text: "Seu texto..." })
+
+    const newState: AppState = { cards: this.cards };
+
+    this.setState(newState)
+  }
+
+  render() {
+
+    return (
+      <section>
+        <main>
+          <header>
+            <h2>Pin Note</h2><img src={iconHorse} />
+          </header>
+
+          <nav>
+            <div className="left">
+              <button onClick={this.insertNote.bind(this)}><IconPlus />Inserir</button>
+            </div>
+            <div className="right">
+              <button><IconMenu2 /></button>
+            </div>
+          </nav>
+
+          <main id="cards">
+            <div>
+              {this.cards.map((card, index) => (
+                <div className="card" key={index}>
+                  <header>{card.title}</header>
+                  <main>{card.text}</main>
+                </div>
+              ))}
+            </div>
+          </main>
+
+          <footer>
+            <h4>Aprendendo React</h4>
+            <p>
+              Software desenvolvido por<br />
+            Conrado Pinheiro &lt;conradopsa@gmail.com&gt;
+            </p>
+          </footer>
+        </main>
+        <aside>
+          <input type='text'></input>
+          Digite para pesquisar por título ou conteúdo.
+
+          <hr />
+
+          Exibir como
+          <button><IconList />Lista</button>
+          <button><IconGrid />Cards</button>
+
+        </aside>
+      </section>
+    );
+  }
 }
 
 export default App;
